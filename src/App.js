8dce82df4ac2301851c28b5ebe1d28f331941e96
Syd <sydneychan2003@gmail.com>
2024-02-04 14:48:09 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          UB ACM
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://discord.gg/dKyK8D27cK"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Join the Discord!
         </a>
       </header>
     </div>
